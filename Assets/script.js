3668de45f7a01b534e939c38f870aa1829266b1e
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-10-31 18:31:03 -0400

    local storage

--- Assets/script.js
+++ Assets/script.js
@@ -6,15 +6,13 @@ const now = moment();
 
 const Time = { text: moment().format("h:00 A"), hour: moment().hour() };
 
-$("#day").text(now.format("dddd MMMM DD "));
+$("#day").text(now.format("dddd MMMM DD[th] "));
 
 var currentTime=moment().hour();
 var allTimeBlocks = document.getElementsByClassName('hour')
 
 for (var i=0;i<allTimeBlocks.length; i++) {
-  // if timeblock < current time
-  //give that timeblock the past color
-  // ParseInt() takes a number and converts it to a string
+
   if (parseInt(allTimeBlocks[i].id) < currentTime) {
     var textArea = allTimeBlocks[i].nextElementSibling
     textArea.classList.add( 'past')
@@ -27,4 +25,12 @@ for (var i=0;i<allTimeBlocks.length; i++) {
     var textArea = allTimeBlocks[i].nextElementSibling
     textArea.classList.add( 'future')
   }
-}
+};
+
+var addButton=document.querySelector("button");
+addButton.addEventListener("click", function() {
+  var textContent=this.previousElementSibling.value;
+  console.log(textContent);
+  localStorage.setItem("textcontent", textContent);
+});
+
